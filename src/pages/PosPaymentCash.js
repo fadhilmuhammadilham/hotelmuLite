1ad6316528bfbe62b2097d8aaf7ef94209d98bc9
Fatah <fatah@kawatama.com>
2022-04-05 13:18:16 +0700

    Merubah perhitungan suggestion pay

--- src/pages/PosPaymentCash.js
+++ src/pages/PosPaymentCash.js
@@ -55,8 +55,9 @@ class PosPaymentCash extends Page {
 
     const sugestionPay = () => {
       if(this.basketService.items.length > 0){
-        var total = this.basketService.totalDiscount > 0 ? Math.round(this.basketService.totalAfterDiscount / 10000.1) * 10000 : Math.round(this.basketService.totalPrice / 10000.1) * 10000
-        total = this.basketService.totalPrice > total ? total + 5000 : total
+        let totalPrices = this.basketService.totalDiscount > 0 ? this.basketService.totalAfterDiscount: this.basketService.totalPrice
+        let total = Math.round(totalPrices / 10000.1) * 10000
+        total = totalPrices > total ? total + 5000 : total
 
         var sugs = [5000, 10000, 20000, 50000, 100000, 150000, 200000, 300000, 500000, 750000, 1000000]
   
@@ -73,10 +74,11 @@ class PosPaymentCash extends Page {
           <a href="javascript:;" class="btn btn-outline-secondary btn-block" data-action="pay-option" data-value="'+sugs[0]+'">'+sugs[0].format()+'</a>\
         </div>')
     
-        $('#uangpas').attr('data-value', this.basketService.totalPrice)
+        $('#uangpas').attr('data-value', totalPrices)
       }else{
-        var total = this.transactionService.totalDiscount > 0 ? Math.round(this.transactionService.totalAfterDiscount / 10000.1) * 10000 : Math.round(this.transactionService.total_prices / 10000) * 10000
-        total = this.transactionService.total_prices > total ? total + 5000 : total
+        let totalPrices = this.transactionService.totalDiscount > 0 ? this.transactionService.totalAfterDiscount: this.transactionService.total_prices
+        let total = Math.round(totalPrices / 10000.1) * 10000
+        total = totalPrices > total ? total + 5000 : total
 
         var sugs = [5000, 10000, 20000, 50000, 100000, 150000, 200000, 300000, 500000, 750000, 1000000]
   
@@ -93,7 +95,7 @@ class PosPaymentCash extends Page {
           <a href="javascript:;" class="btn btn-outline-secondary btn-block" data-action="pay-option" data-value="'+sugs[0]+'">'+sugs[0].format()+'</a>\
         </div>')
     
-        $('#uangpas').attr('data-value', this.transactionService.total_prices)
+        $('#uangpas').attr('data-value', totalPrices)
       }
   
       
@@ -101,7 +103,7 @@ class PosPaymentCash extends Page {
 
     if ($('#payment-suggestion').length > 0) sugestionPay()
 
-		$(document).on('click', 'a[data-action="other-nominal"]', () => {
+		$('a[data-action="other-nominal"]').on('click', () => {
 			$('#payment-nominal').collapse('show')
 			$('#payment-nominal input').val(0)
 
@@ -111,7 +113,7 @@ class PosPaymentCash extends Page {
       checkValue()
 		})
 
-		$(document).on('click', '[data-action="pay-option"]', (e) => {
+		$('[data-action="pay-option"]').on('click', (e) => {
 			$('#payment-nominal').collapse('hide')
 			var val = $(e.currentTarget).data('value')
 
@@ -124,7 +126,7 @@ class PosPaymentCash extends Page {
 			checkValue()
 		})
 
-		$(document).on('keyup', '#payment_nominal', (e) => {
+		$('#payment_nominal').on('keyup', (e) => {
 			var val = $(e.currentTarget).val()
 			payment_value = parseFloat(val)
 
@@ -145,15 +147,6 @@ class PosPaymentCash extends Page {
             discount: parseFloat(this.basketService.discount.discount),
             discount_note: parseInt(BasketLocalStorage.get('discount').discount_note)
           })
-          let res = await TransactionApi.save()
-  
-          if(res.status) {
-            let payment = await TransactionApi.payment(res.data)
-  
-            if(payment.status) {
-              Redirect('/pos/payment/finish')
-            }
-          }
         }else{
           this.basketService.setPayment({
             payment_method: 0,
@@ -163,14 +156,16 @@ class PosPaymentCash extends Page {
             total_payment: payment_value,
             refund: refund,
           })
-          let res = await TransactionApi.save()
-  
-          if(res.status) {
-            let payment = await TransactionApi.payment(res.data)
-  
-            if(payment.status) {
-              Redirect('/pos/payment/finish')
-            }
+        }
+        
+        let res = await TransactionApi.save()
+
+        if(res.status) {
+          let payment = await TransactionApi.payment(res.data)
+
+          if(payment.status) {
+            $('#pay-modal').modal('hide')
+            Redirect('/pos/payment/finish')
           }
         }
       }else{
@@ -186,15 +181,6 @@ class PosPaymentCash extends Page {
             discount: parseFloat(this.transactionService.discount.discount),
             discount_note: parseInt(TransactionLocalStorage.get('discount').discount_note)
           })
-
-          // console.log(this.transactionService.payment);
-
-          let res = await TransactionApi.payment(TransactionLocalStorage.get('id'))
-
-          if(res.status) {
-            Redirect('/pos/payment/finish')
-          }
-
         }else{
           this.transactionService.setPayment({
             payment_method: 0,
@@ -204,14 +190,13 @@ class PosPaymentCash extends Page {
             total_payment: payment_value,
             refund: refund,
           })
+        }
 
-          // console.log(this.transactionService.payment);
+        let res = await TransactionApi.payment(TransactionLocalStorage.get('id'))
 
-          let res = await TransactionApi.payment(TransactionLocalStorage.get('id'))
-
-          if(res.status) {
-            Redirect('/pos/payment/finish')
-          }
+        if(res.status) {
+          $('#pay-modal').modal('hide')
+          Redirect('/pos/payment/finish')
         }
       }
     }
