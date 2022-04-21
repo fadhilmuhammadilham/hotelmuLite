89a2213a7fa3de0779afd8ae5633e29c62c8b43c
Fatah <fatah@kawatama.com>
2022-04-21 10:50:49 +0700

    menambah enpoint opened pada repo shiftApi

--- src/repositories/api/ShiftApi.js
+++ src/repositories/api/ShiftApi.js
@@ -2,53 +2,69 @@ import API from "../../configs/ApiConfig";
 import { getCookie } from "../../core/Cookies";
 
 class ShiftApi {
-    static async open(begining_balance) {
-        let url = `${API.url}/resto/shift/open`;
-        let bearer = 'Bearer ' + getCookie('token');
+	static async open(begining_balance) {
+		let url = `${API.url}/resto/shift/open`;
+		let bearer = 'Bearer ' + getCookie('token');
 
-        try {
-            let response = await fetch(url, {
-                method: 'POST',
-                headers: {
-                    'Authorization': bearer,
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                },
-                mode: 'cors',
-                body: JSON.stringify({ begining_balance: begining_balance })
-            })
-    
-            let json = await response.json();
-    
-            return json
-        } catch (error) {
-            console.log(error);
-        }
+		try {
+			let response = await fetch(url, {
+				method: 'POST',
+				headers: {
+					'Authorization': bearer,
+					'Accept': 'application/json',
+					'Content-Type': 'application/json'
+				},
+				mode: 'cors',
+				body: JSON.stringify({ begining_balance: begining_balance })
+			})
 
-    }
+			let json = await response.json();
 
-    static async close(id) {
-        let url = `${API.url}/resto/shift/${id}/close`;
-        let bearer = 'Bearer ' + getCookie('token');
+			return json
+		} catch (error) {
+			console.log(error);
+		}
+	}
 
-        try {
-            let response = await fetch(url, {
-                method: 'PUT',
-                headers: {
-                    'Authorization': bearer,
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json'
-                },
-            })
-    
-            let json = await response.json();
-    
-            return json
-        } catch (error) {
-            console.log(error);
-        }
+	static async close(id) {
+		let url = `${API.url}/resto/shift/${id}/close`;
+		let bearer = 'Bearer ' + getCookie('token');
 
-    }
+		try {
+			let response = await fetch(url, {
+				method: 'PUT',
+				headers: {
+					'Authorization': bearer,
+					'Accept': 'application/json',
+					'Content-Type': 'application/json'
+				},
+			})
+
+			let json = await response.json();
+
+			return json
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	static async opened() {
+    let url = `${API.url}/resto/shift/opened`;
+    let bearer = 'Bearer ' + getCookie('token')
+
+    let response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Authorization' : bearer,
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+    })
+
+    let json = await response.json();
+
+    return json;
+  }
 }
 
 export default ShiftApi
