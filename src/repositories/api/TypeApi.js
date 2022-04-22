f7caf78e3b363a5e4b4933ffa5611b4c5d9cacb3
Fatah <fatah@kawatama.com>
2022-04-22 14:06:37 +0700

    Menghapus kode yang di komen

--- src/repositories/api/TypeApi.js
+++ src/repositories/api/TypeApi.js
@@ -3,33 +3,19 @@ import API from "../../configs/ApiConfig";
 
 class TypeApi {
   static async getAll() {
-    // const types = [
-    //   {id: 1, name: 'Dine In'},
-    //   {id: 2, name: 'Room Service'},
-    //   {id: 3, name: 'Bunquet'},
-    // ]
-
-    // let proccess = new Promise(resolve => setTimeout(() => resolve({
-    //   status: true,
-    //   data: types,
-    //   message: "Data type berhasil diakses"
-    // }), 500))
-
-    // return await proccess
-
     let url = `${API.url}/resto/outlet`;
     let bearer = "Bearer " + getCookie('token')
 
     try {
       let response = await fetch(url, {
-      method: 'GET',
+        method: 'GET',
         headers: {
-            'Authorization': bearer,
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
+          'Authorization': bearer,
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
         },
       });
-  
+
       return await response.json();
     } catch (error) {
       console.log(error);
